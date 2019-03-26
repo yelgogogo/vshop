a86ba0502d1b8b471a4c4c540b29598361c75904
yelgogogo <yelgogogo@hotmail.com>
2019-03-26 22:09:53 +0800

    改正接收api的数据

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let util = {}
 // 8688
-const ajaxUrl = process.env.NODE_ENV === 'development' ? 'http://120.25.86.111:89/' : process.env.NODE_ENV === 'production' ? 'http://120.25.86.111:89/' : 'https://debug.url.com'
+const ajaxUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:81/' : process.env.NODE_ENV === 'production' ? 'http://120.25.86.111:89/' : 'https://debug.url.com'
 
 util.ajax = axios.create({
   baseURL: ajaxUrl,
