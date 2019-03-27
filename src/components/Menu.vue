86f3e03a8a06c8db953e76b11289750bcedecfd4
yelgogogo <yelgogogo@hotmail.com>
2019-03-27 21:38:57 +0800

    update

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,82 +1,30 @@
-<template>
-  <div class="mainBox">
-    <div class="leftBox">
-      <div
-        v-for="(type,index) in GoodsTypes"
-        :key="type" class="leftBoxItem"
-        :class="{'current':currentIndex === index}"
-        @click="selectType(type, index)">
-        {{type}}
-      </div>
-    </div>
-    <div class="rightBox">
-        <div v-for="good in goods" :key="good.ID" class="goodCard">
-          <img
-            src="https://img.4008123123.com/resource/VersionP/phdi/288_224482.jpg"
-            alt="" width="100" height="100">
-          <div class="footer">
-            <div>{{good.GoodsName}}</div>
-            <div class="price">￥{{good.Price}}</div>
-          </div>
-
-        </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import Util from '../libs/utils.js'
-export default {
-  name: 'Menu',
-  data () {
-    return {
-      types: [1, 2, 3, 4, 5],
-      currentIndex: 0,
-      goods: [],
-      foods: [],
-      GoodsTypes: []
-    }
-  },
-  mounted () {
-    // this.getTypes()
-    // this.selectType(this.GoodsTypes[0], 0)
-    this.loadFoods()
-  },
-  methods: {
-    loadFoods () {
-      let apiURL = 'webserviceex.asmx/Moon_GetLocalGoods'
-      Util.ajax.get(apiURL).then(res => {
-		console.log(res)
-        this.foods = res.data
-        this.getTypes()
-        this.selectType(this.GoodsTypes[0], 0)
-      })
-    },
-    selectType (type, index) {
-      this.currentIndex = index
-      this.goods = this.foods.filter(f => f.GoodsTypeName === type)
-		console.log(type)
-    },
-    getTypes () {
-      let arr = this.foods.map(f => f.GoodsTypeName)
-      this.GoodsTypes = Array.from(new Set(arr))
-    }
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .mainBox {
   display: flex;
+  flex-direction: column;
+}
+.topBox {
+  height: 50px;
+  background-color: rgb(1, 1, 1);
+}
+.bottomBox {
+  height: 1px;
+}
+.centerBox {
+  display: flex;
 }
 .leftBox {
   width: 200px;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  background-color: rgb(26, 25, 24);
 }
 .rightBox {
-  flex:1;
+  flex: 1;
+  height: 100%;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -112,3 +60,72 @@ export default {
   width: 60px;
 }
 </style>
+
+<template>
+  <div class="mainBox">
+    <div class="topBox">top</div>
+    <div class="centerBox">
+      <div class="leftBox">
+        <div v-for="(type, index) in GoodsTypes"
+             :key="type"
+             class="leftBoxItem"
+             :class="{ current: currentIndex === index }"
+             @click="selectType(type, index)">
+          {{ type }}
+        </div>
+      </div>
+      <div class="rightBox">
+        <div v-for="good in goods"
+             :key="good.ID"
+             class="goodCard">
+          <img src={{apiURL+good.ID+'.jpg'}}
+               alt
+               width="100"
+               height="100" />
+          <div class="footer">
+            <div>{{ good.GoodsName }}</div>
+            <div class="price">￥{{ good.Price }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottomBox">bottom</div>
+  </div>
+</template>
+rainforce1982@sina.com
+<script>
+import Util from '../libs/utils.js'
+export default {
+  name: 'Menu',
+  data () {
+    return {
+      types: [1, 2, 3, 4, 5],
+      currentIndex: 0,
+      goods: [],
+      foods: [],
+      GoodsTypes: []
+    }
+  },
+  mounted () {
+    this.loadFoods()
+  },
+  methods: {
+    loadFoods () {
+      let apiURL = 'webserviceex.asmx/Moon_GetLocalGoods'
+      Util.ajax.get(apiURL).then(res => {
+        this.foods = res.data
+        this.getTypes()
+        this.selectType(this.GoodsTypes[0], 0)
+      })
+    },
+    selectType (type, index) {
+      this.currentIndex = index
+      this.goods = this.foods.filter(f => f.GoodsTypeName === type)
+    },
+    getTypes () {
+      let arr = this.foods.map(f => f.GoodsTypeName)
+      this.GoodsTypes = Array.from(new Set(arr))
+    }
+  }
+}
+</script>
